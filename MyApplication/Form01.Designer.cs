6cca7b56af89c6aef84e0886447d30b6b35929be
zeinab <zeynabsamany@gmail.com>
2021-06-09 22:58:56 +0430

    Add Button, TextBox, Label on Form 1 and Form 6

--- MyApplication/Form01.Designer.cs
+++ MyApplication/Form01.Designer.cs
@@ -28,18 +28,56 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.SuspendLayout();
-			// 
-			// Form01
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(484, 461);
-			this.Location = new System.Drawing.Point(0, 0);
-			this.Name = "Form01";
-			this.ResumeLayout(false);
+            this.testTextBox = new System.Windows.Forms.TextBox();
+            this.testLabel = new System.Windows.Forms.Label();
+            this.testButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // testTextBox
+            // 
+            this.testTextBox.Location = new System.Drawing.Point(212, 81);
+            this.testTextBox.Name = "testTextBox";
+            this.testTextBox.Size = new System.Drawing.Size(178, 24);
+            this.testTextBox.TabIndex = 5;
+            // 
+            // testLabel
+            // 
+            this.testLabel.AutoSize = true;
+            this.testLabel.Location = new System.Drawing.Point(69, 88);
+            this.testLabel.Name = "testLabel";
+            this.testLabel.Size = new System.Drawing.Size(107, 17);
+            this.testLabel.TabIndex = 4;
+            this.testLabel.Text = "zeinab samani";
+            // 
+            // testButton
+            // 
+            this.testButton.Location = new System.Drawing.Point(72, 171);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(151, 39);
+            this.testButton.TabIndex = 3;
+            this.testButton.Text = "zeinab samani";
+            this.testButton.UseVisualStyleBackColor = true;
+            // 
+            // Form01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 603);
+            this.Controls.Add(this.testTextBox);
+            this.Controls.Add(this.testLabel);
+            this.Controls.Add(this.testButton);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MinimumSize = new System.Drawing.Size(509, 248);
+            this.Name = "Form01";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
-		#endregion /Windows Form Designer generated code
-	}
+        #endregion /Windows Form Designer generated code
+
+        private System.Windows.Forms.TextBox testTextBox;
+        private System.Windows.Forms.Label testLabel;
+        private System.Windows.Forms.Button testButton;
+    }
 }
--- MyApplication/Form06.Designer.cs
+++ MyApplication/Form06.Designer.cs
@@ -27,24 +27,56 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.SuspendLayout();
+            this.testButton = new System.Windows.Forms.Button();
+            this.testLabel = new System.Windows.Forms.Label();
+            this.testTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // testButton
+            // 
+            this.testButton.Location = new System.Drawing.Point(112, 217);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(151, 39);
+            this.testButton.TabIndex = 0;
+            this.testButton.Text = "zeinab samani";
+            this.testButton.UseVisualStyleBackColor = true;
+            // 
+            // testLabel
+            // 
+            this.testLabel.AutoSize = true;
+            this.testLabel.Location = new System.Drawing.Point(109, 134);
+            this.testLabel.Name = "testLabel";
+            this.testLabel.Size = new System.Drawing.Size(107, 17);
+            this.testLabel.TabIndex = 1;
+            this.testLabel.Text = "zeinab samani";
+            // 
+            // testTextBox
+            // 
+            this.testTextBox.Location = new System.Drawing.Point(252, 127);
+            this.testTextBox.Name = "testTextBox";
+            this.testTextBox.Size = new System.Drawing.Size(178, 24);
+            this.testTextBox.TabIndex = 2;
+            // 
+            // Form06
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 603);
+            this.Controls.Add(this.testTextBox);
+            this.Controls.Add(this.testLabel);
+            this.Controls.Add(this.testButton);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MinimumSize = new System.Drawing.Size(509, 248);
+            this.Name = "Form06";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
-			this.AutoScaleDimensions =
-				new System.Drawing.SizeF(7F, 13F);
-
-			this.AutoScaleMode =
-				System.Windows.Forms.AutoScaleMode.Font;
-
-			this.ClientSize =
-				new System.Drawing.Size(484, 461);
-
-			this.Location =
-				new System.Drawing.Point(0, 0);
-
-			this.Name = "Form06";
-
-			this.ResumeLayout(false);
 		}
-		#endregion /Windows Form Designer generated code
-	}
+        #endregion /Windows Form Designer generated code
+
+        private System.Windows.Forms.Button testButton;
+        private System.Windows.Forms.Label testLabel;
+        private System.Windows.Forms.TextBox testTextBox;
+    }
 }
